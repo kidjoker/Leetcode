0fd0c7e38ef1f8fd7ac431ce1e61cb00bcf23829
kidjoker <superherojoke@gmail.com>
2024-02-28 18:32:19 -0600

    反转链表

---
+++ 206.reverse-linked-list.js
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode id=206 lang=javascript
+ *
+ * [206] Reverse Linked List
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+    let prev = null;
+    let cur = head;
+    while(cur !== null) {
+        let temp = cur.next;
+        cur.next = prev;
+        prev = cur;
+        cur = temp;
+    }
+
+    return prev;
+};
+// @lc code=end
+
